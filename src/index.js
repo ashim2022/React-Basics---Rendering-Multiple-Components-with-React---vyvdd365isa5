c1157db30b16b4dd0f0978b6ca6ecdc5adfe279a
Ashim <shawashim72@gmail.com>
2022-06-19 21:52:03 +0530

    starter files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
+console.log(React)
 
 ReactDOM.render(<App />, document.getElementById("root"));
